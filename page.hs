069246868a8a30d99f6cca15776fae66e0cd6df3
Thomas Levine <_@thomaslevine.com>
2014-09-30 17:59:46 +0000

    page.hs

---
+++ page.hs
@@ -0,0 +1,11 @@
+    The lock-byte page
+    A freelist page
+        A freelist trunk page
+        A freelist leaf page 
+    A b-tree page
+        A table b-tree interior page
+        A table b-tree leaf page
+        An index b-tree interior page
+        An index b-tree leaf page 
+    A payload overflow page
+    A pointer map page 
